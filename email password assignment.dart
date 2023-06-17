79bb8fce968378741b4c8aebb09a54e98e0e5bd9
shahid <hmshahid2003@gmail.com>
2023-06-17 11:45:40 +0500

    first commit

---
+++ email password assignment.dart
@@ -0,0 +1,26 @@
+void main () {
+var email = "hmshahid2003@gmail.com";
+var password = "abc123";
+
+if (email == "hmshahid2002@gmail.com" && password == "abc123") {
+  print ('you are logged in');
+
+}
+
+else if (email == "hmshahid2003@gmail.com" || password != "abc123" )
+
+{
+  print ('password is wrong');
+}
+ else if (email != "hmshahid2003@gmail.com" || password == "abc123" )
+  print ('email is wrong');
+
+else {
+
+ print ('email and password both are wrong');
+
+}
+
+
+
+}
